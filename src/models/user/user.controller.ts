b96086b1b4298a4465f5ed499eacda20f34705e6
Leonardo <leowitcroz@gmail.com>
2024-07-16 19:56:52 -0300

    update e delete

--- src/models/user/user.controller.ts
+++ src/models/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 
 @Controller('user')
@@ -20,5 +20,15 @@ export class UserController {
     async createUser(@Body() { email, password }) {
         return this.user.createUser({ email, password })
     }
+
+    @Patch(':id')
+    async updateUser(@Param('id', ParseIntPipe) id, @Body() {email, password}){
+        return this.user.updateUser(id,{email,password})
+    }
+    @Delete(':id')
+    async deleteUser(@Param('id', ParseIntPipe) id){
+        return this.user.delete(id)
+    }
+    
 
 }
--- src/models/user/user.service.ts
+++ src/models/user/user.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../../prisma/prisma.service';
+import { user } from '@prisma/client';
 
 @Injectable()
 export class UserService {
@@ -24,5 +25,40 @@ export class UserService {
                 password: password
             }
         })
+    }
+
+    async updateUser(id: number, { email, password }) {
+        await this.exist(id)
+
+        const data: any = {}
+
+        if (email) {
+            data.email = email;
+        }
+        if (password) {
+            data.password = password
+        }
+
+        return this.prisma.user.update({
+            where: {
+                id
+            },
+            data
+        })
+    }
+
+    async delete(id:number) {
+        await this.exist(id)
+        return this.prisma.user.delete({
+            where:{
+                id
+            }
+        })
+    }
+
+    async exist(id: number) {
+        if (!(await this.findUnique(id))) {
+            throw new NotFoundException('The user doesnt exist');
+        }
     }
 }
